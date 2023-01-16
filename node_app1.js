98ae208719cb07d15a1687db1d5b15bffdaa3832
tasospim <tassospim@gmail.com>
2023-01-16 21:44:36 +0200

    After Nodejs Express Instalation

--- node_app1.js
+++ node_app1.js
@@ -1,9 +1,24 @@
-const express = require("express");
+const express = require('express');
+//
+//
+console.log("Hello Tasos");
+
+var moment = require('moment');
+var date = moment().format('LL');
+console.log(date);
+
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = 3000;
 
-// your code
-
-app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+app.get('/', (req, res)=>{
+	res.status(200);
+	res.send("Welcome to root URL of Server  TASOS POIMENIDIS  copyright ");
 });
+
+app.listen(PORT, (error) =>{
+	if(!error)
+		console.log("Server is Successfully Running, and App is listening on port "+ PORT);
+	else
+		console.log("Error occurred, server can't start", error);
+	}
+);
